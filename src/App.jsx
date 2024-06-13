916a8cdb61df0c0fc3543227518611f491038416
Kacper <k.olszak25@gmail.com>
2024-06-13 21:16:21 +0200

    static site created (mobile only)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import './App.module.css'
+import styles from './App.module.css'
 
 function App() {
-  
+	return (
+		<>
+      <div className={styles.background}>
+			<header className={styles.titleBackground}>
+				<h1 className={styles.siteTitle}>FISZKONATOR</h1>
+			</header>
 
-  return (
-    <>
-    </>
-  )
+      <main className={styles.setOfFlashcardsContainer}>
+        <div className={styles.setOfFlashcards}></div>
+        <div className={styles.setOfFlashcards}></div>
+        <div className={styles.setOfFlashcards}></div>
+        <div className={styles.setOfFlashcards}></div>
+        <div className={styles.setOfFlashcards}></div>
+        <div className={styles.setOfFlashcards}></div>
+        <div className={styles.setOfFlashcards}></div>
+      </main>
+
+      </div>
+		</>
+	)
 }
 
 export default App
