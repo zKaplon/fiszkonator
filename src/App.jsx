e58cf8133e95e1dae2b6f38a75f577d915639fbe
Zuzia <zuzu.kaplon@gmail.com>
2024-06-14 12:33:57 +0200

    added media queries, unified design

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 				<h1 className={styles.siteTitle}>FISZKONATOR</h1>
 			</header>
 
-      <main className={styles.setOfFlashcardsContainer}>
+      <main className={styles.setsOfFlashcardsContainer}>
       <SetOfFlashcards></SetOfFlashcards>
       <SetOfFlashcards></SetOfFlashcards>
       <SetOfFlashcards></SetOfFlashcards>
